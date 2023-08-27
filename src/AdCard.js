83078aee9309d7e07f001089d09129a95d0ccde3
Allen Kim <avalmercafe02@gmail.com>
2023-08-27 19:10:55 -0400

    added results to the ads from googly analytics

--- src/AdCard.js
+++ src/AdCard.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-function AdCard({campaignName,mediaBuyName,adName,cost,clicks}) {
+function AdCard({campaignName,mediaBuyName,adName,cost,clicks,results}) {
+ 
     return (
         <div className="w-[25%] aspect-w-1 aspect-h-1 bg-gray-200 border border-gray-300">
             <h1> {adName} </h1>
             <h1> Campaign {campaignName} </h1>
             <h2> Media Buy Name: {mediaBuyName} </h2>
+            <h3> Results: {results}</h3>
             <div className="px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0 border-t-black border-b-black">
                 <dt className="text-lg font-medium">
                     Cost
