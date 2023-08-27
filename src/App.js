384e7343ee21810d4277710a2192a8fd3e65b18f
Allen Kim <avalmercafe02@gmail.com>
2023-08-27 18:05:06 -0400

    modified data to have same key

--- src/App.js
+++ src/App.js
@@ -15,7 +15,28 @@ function App() {
     setAdvertisementData(combinedArray)})
   },[])
   const [advertisementData, setAdvertisementData] = useState([])
-  console.log(advertisementData)
+ const keyMapping = {
+  "campaign_name" : "campaign",
+  "utm_campaign" : "campaign"
+,  "media_buy_name" : "ad_group",
+  "ad_squad_name" : "ad_group",
+  "utm_medium" : "ad_group"
+
+ }
+ const modifiedData = advertisementData.map((ad)=> {
+  const modifiedAd = {}
+  for (const key in ad) {
+    if (keyMapping[key]){
+      modifiedAd[keyMapping[key]] = ad[key]
+    }
+    else {
+      modifiedAd[key] = ad[key]
+    }
+  }
+  return modifiedAd
+ })
+ console.log(advertisementData, "adData")
+ console.log(modifiedData, "modifiedData")
   return (
     <div className="bg-primary w-full h-full">
         <CardContainer adData={advertisementData}/>
